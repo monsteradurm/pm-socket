93a3f6ed0c500053b096a05a986e23c886ea05b8
Adam Cranch <acranch@liquidanimation.com>
2021-05-28 01:29:39 +1000

    routes

--- app.js
+++ app.js
@@ -6,18 +6,29 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 io.on("connection", socket => {
-    // ...
-    socket.on('boarditem-update', (update) => {
-        io.emit('boarditem-update', update);
-    });
-
-    socket.on('syncketch-update', (update) => {
-        io.emit('syncsketch-update', update);
-    });
-
-    socket.on('disconnect', () => {});
+  socket.on('test-message', (update) => {
+    io.emit("test-message", update);
   });
-  
-  server.listen(3000, () => {
-    console.log('listening on *:3000');
+
+  socket.on('send-issue-update', (update) => {
+    io.emit('issue-update', update);
+  })
+  socket.on('send-boarditem-update', (update) => {
+
+    io.emit('boarditem-update', update);
   });
+
+  socket.on('send-board-update', (update) => {
+    io.emit('board-update', update);
+  });
+
+  socket.on('send-syncketch-update', (update) => {
+    io.emit('syncsketch-update', update);
+  });
+
+  socket.on('disconnect', () => { });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
